fb90aab999f3293e240a39b0dbbeb9807c4dc91d
nageshreddy2706 <nageshreddy1988@gmail.com>
2021-03-24 15:11:51 +0530

    Adding a shell script for variables demo

--- 03-variables.sh
+++ 03-variables.sh
@@ -9,3 +9,28 @@ COURSE_NAME=DevOps
 echo welcome to $COURSE_NAME training
 echo $COURSE_NAME Training Timings - 6AM
 echo $COURSE_NAME Trainer - Nagesh
+
+
+# Declare a variable, VARNAME=DATA
+# To access variable, $VARNAME
+
+# what characters you can use in variables
+# A-Z, a-z, 0-9 , _(underscore)
+
+# variable name should not start with a number
+      # 1var is wrong
+      # var1 is right
+
+# STYLE in variable NAMES
+# Unix/Linux  -> VARNAME (All in capital characters_
+# Java - varname (camelcase)
+
+DATE=2021-24-03
+echo "Good Morning,  Today date is $DATE"
+
+# To declare variable content dynamically then we should be using
+    # 1. Command substitution   VAR=$(command)
+    # 2. Arithmetic substitution VAR=$((exopression))
+
+DATE=$(date +%F)
+echo "Good Morning, Today date is $DATE"
