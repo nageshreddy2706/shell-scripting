ffb0ffaf1e78c022af467f6facef25ada9b1c6a8
nageshreddy2706 <nageshreddy1988@gmail.com>
2021-03-24 14:59:36 +0530

    Adding a shell script for variables demo

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo welcome to DevOps training
+echo DevOps Training Timings - 6AM
+echo DevOps Trainer - Nagesh
+
+COURSE_NAME=DevOps
+
+echo welcome to $COURSE_NAME training
+echo $COURSE_NAME Training Timings - 6AM
+echo $COURSE_NAME Trainer - Nagesh
