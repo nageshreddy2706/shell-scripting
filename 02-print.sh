f3fd5fae8d87963863d86ee02efb0a97c6dad096
nageshreddy2706 <nageshreddy1988@gmail.com>
2021-03-24 14:33:33 +0530

    Adding a shell script for printf demo

---
+++ 02-print.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# we can print a message on screen using two commands in linux
+# 1. echo  -> preferred one, because of less syntax
+# 2. printf  -> Have more syntax
+
+echo Hello world
