e42a206d8c4ee7423fefa918212ca9c08f0c110f
nageshreddy2706 <nageshreddy1988@gmail.com>
2021-03-25 16:29:53 +0530

    Adding functions.sh

---
+++ 04-functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Assign a name to data is a variable
+# Assign a name to group of commands is called as fucntion
+
+# Declare a function
+
+# way 1
+fucntion SAMPLE() {
+  echo Hello from sample function
+}
+
+# way 2
+SAMPLE1() {
+  echo Hello from sample1 function
+  }
+
+#Function name standards are as same as variables in bash shell
+
+# call a function
+SAMPLE
+SAMPLE1
